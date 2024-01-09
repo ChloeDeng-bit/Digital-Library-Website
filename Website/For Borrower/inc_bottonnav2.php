6ea074820fd8ff50ae0fd685e8f35929dea1f10f
meiyun deng <127011014+meiyunDeng@users.noreply.github.com>
2024-01-09 23:25:42 +1100

    Add files via upload

---
+++ Website/For Borrower/inc_bottonnav2.php
@@ -0,0 +1,17 @@
+
+<table border='0' width='500%'>
+    <tr>
+        <th style="width:300px;font-size:20px">
+        <a href='<?php echo "borrower.php?". SID ."&content=myBorrow"?>'>my borrowed resource</a>
+        </th>
+        <th style="width:300px;font-size:20px">
+        <a href='<?php echo "borrower.php?". SID ."&content=in_borrow"?>'>resource in borrow</a>         
+        </th>
+        <th style="width:300px;font-size:20px">
+        <a href='<?php echo "borrower.php?". SID ."&content=search"?>'>search resource</a>           
+        </th>
+        <!-- <th style="width:300px;font-size:20px">
+        <a href='<?php echo "borrower.php?". SID ."&content=insert"?>'>insert resource</a>         
+        </th> -->
+    </tr>
+</table>
