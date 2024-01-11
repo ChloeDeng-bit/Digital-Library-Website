48fb4874911e9b789441b0c49533342ff15078f3
meiyun deng <127011014+meiyunDeng@users.noreply.github.com>
2024-01-11 16:01:00 +1100

    Update insert.php

--- Website/For Librarian/insert.php
+++ Website/For Librarian/insert.php
@@ -1,29 +1,44 @@
+<!-- This file is for librarian to insert new book resource,
+it will check if all the book information has been input and if the book exist before save in the database. -->
+
 <?php
-//session_start();
+session_start();
 
 $error=0;
 $Body="";
 $input=true;
 $repeat=false;
 $allInfor=true;
-if(isset($_POST['submit'])){ // Check if form was submitted
+
+// Check if the book information has been submitted and save them
+if(isset($_POST['submit'])){ 
 
     $ISBN = $_POST['ISBN']; 
     $title = $_POST['title']; 
     $author = $_POST['author']; 
-    $publisher = $_POST['publisher']; 
-    //$status = $_POST['status']; 
+    $publisher = $_POST['publisher'];  
     $cost_per_day = $_POST['cost_per_day']; 
-    //$message = $ISBN .$title.$author.$publisher.$status.$cost_per_day;
-    //echo $message;
     $input=false;
 }
+
+//keep the session
+if (isset($_SESSION['userID'])){
+
+    $userID = $_SESSION['userID'];
+}
+else {
+    $Body .= "<p>You have not logged in or registered. Please return to the <a href='registerAndLogin.php'>Registration / Log In page</a>.</p>";
+    ++$error;
+    echo "$error";
+}
+
+//check if the book exist already or not
 if(!empty($ISBN)){
     include("inc_digitalLibrary.php");
     $TableName = "resource";
     $sql="SELECT * FROM $TableName WHERE ISBN='".$ISBN."'";
     $req=mysqli_query($conn, $sql);
-    //$Row = mysqli_fetch_row($req);
+
     if (mysqli_fetch_row($req)!==null) {
         echo "<p>This book (" . htmlentities($ISBN) . ") has already in the resource.</p>\n";
         $repeat=true;
@@ -31,6 +46,8 @@ if(!empty($ISBN)){
     }
 }
 
+//if the book is not exist in the database and all the book information has been saved successfully 
+//save the add this book information into resource table
 if((!empty($ISBN))&&(!empty($title))&&(!empty($author))&&(!empty($publisher))&&(!empty($cost_per_day)&&($repeat==false))){
     $allInfor=true;
     include("inc_digitalLibrary.php");
@@ -45,21 +62,23 @@ if((!empty($ISBN))&&(!empty($title))&&(!empty($author))&&(!empty($publisher))&&(
        die(mysqli_error($conn));
    }
 
+//else echo the warning
 }else if($allInfor==false){
     echo "please fill up all the information required";
 }
 
-if($input==true){ 
+//if the information haven't been input then display the form for librarian to input book information
+if($input==true)&&($error==0){ 
      $Body .="<p>Please enter the detail of the resource.</p>";
      $Body .='<form method="post" action="" >';
      $Body .='ISBN: <input type="text" name="ISBN" />&nbsp;&nbsp;';
      $Body .='Title: <input type="text" name="title" />&nbsp;&nbsp';
      $Body .='Author: <input type="text" name="author" /><br/><br/>';
      $Body .='Publisher: <input type="text" name="publisher" />&nbsp;&nbsp';
-     //$Body .='Status: <input type="text" name="status" />&nbsp;&nbsp';
      $Body .='Cost per day: <input type="text" name="cost_per_day" />';
      $Body .='<p><input type="submit" name="submit" value="submit" /></p>';
      $Body .='</form>';
  }
  
      echo $Body;
+     ?>
